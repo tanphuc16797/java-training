33188c7d37dbf39e8ec6f4e9d85abe10dcebd48c
tanphuc16797 <54257990+tanphuc16797@users.noreply.github.com>
2021-11-28 16:16:28 +0700

    Delete restapi directory

--- restapi/src/main/java/com/amit/spring/model/request/CreateStudentRequest.java
+++
@@ -1,9 +0,0 @@
-package com.amit.spring.model.request;
-
-import lombok.Data;
-
-@Data
-public class CreateStudentRequest {
-    private String name;
-    private Integer classId;
-}
--- restapi/src/main/java/com/amit/spring/model/request/DeteleClassRequest.java
+++
@@ -1,8 +0,0 @@
-package com.amit.spring.model.request;
-
-import lombok.Data;
-
-@Data
-public class DeteleClassRequest {
-    private int id;
-}
